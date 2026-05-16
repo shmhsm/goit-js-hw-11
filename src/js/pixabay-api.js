25f5248b74ca5b63539eadd5f035943007dff482
shmhsm <shm.ughh@gmail.com>
2026-05-16 23:06:37 +0200

    feat: complete homework 11 image search

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '55893053-f41b99238ee3ffa17075fed9a';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
+}
